ee0640c8a541feb591745071bc0188f69f929ded
anubhav1309 <rudra130906@gmail.com>
2026-04-29 10:47:16 +0530

    Implemented UC3 Accept User Slot Input

--- java/TicTacToe.java
+++ java/TicTacToe.java
@@ -1,50 +1,24 @@
-import java.util.Random;
 import java.util.Scanner;
 
-public class TicTacToeUC2 {
+public class TicTacToeUC3 {
 
-    static char player1Symbol;
-    static char player2Symbol;
-    static int currentPlayer;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+        System.out.println("=== TIC TAC TOE ===");
 
-        System.out.println("=== TIC TAC TOE GAME ===");
-        System.out.print("Enter Player 1 Name: ");
-        String player1 = scanner.nextLine();
+        int slot = getUserSlotInput();
 
-        System.out.print("Enter Player 2 Name: ");
-        String player2 = scanner.nextLine();
+        System.out.println("Player selected slot: " + slot);
+    }
 
-        // Toss to decide first player
-        int toss = random.nextInt(2); // 0 or 1
+    // Method to accept user slot input
+    public static int getUserSlotInput() {
 
-        if (toss == 0) {
-            currentPlayer = 1;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-        } else {
-            currentPlayer = 2;
-            player1Symbol = 'O';
-            player2Symbol = 'X';
-        }
+        System.out.print("Enter slot number (1-9): ");
+        int slot = scanner.nextInt();
 
-        // Display toss result
-        System.out.println("\n=== Toss Result ===");
-
-        if (currentPlayer == 1) {
-            System.out.println(player1 + " won the toss.");
-            System.out.println(player1 + " plays first with symbol " + player1Symbol);
-            System.out.println(player2 + " gets symbol " + player2Symbol);
-        } else {
-            System.out.println(player2 + " won the toss.");
-            System.out.println(player2 + " plays first with symbol " + player2Symbol);
-            System.out.println(player1 + " gets symbol " + player1Symbol);
-        }
-
-        scanner.close();
+        return slot;
     }
 }
